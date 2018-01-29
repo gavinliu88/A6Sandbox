3044196b344c03311d020ec26d9f0872758158d2
Gavin Liu <gavin.liu@surrey.ca>
2018-01-29 10:51:24 -0800

    Initial checkin

---
+++ Package_bodies/git_demo_body.sql
@@ -0,0 +1,10 @@
+create or replace PACKAGE BODY        GIT_DEMO AS
+
+  Function f1(rate float)
+  RETURN float AS
+  BEGIN
+    -- TODO: Initial checkin
+    RETURN NULL;
+  END f1;
+
+END GIT_DEMO;
---
+++ Packages/git_demo.sql
@@ -0,0 +1,8 @@
+create or replace PACKAGE        GIT_DEMO AS 
+
+
+Function f1(rate float)
+RETURN float;
+
+
+END GIT_DEMO;
