498bf708a8f45625afc25aadeafb1e706e9ea67d
gavinliu88 <gavin.liu@surrey.ca>
2018-02-02 20:39:24 -0800

    Evan change something

--- Package_bodies/git_demo_body3.sql
+++ Package_bodies/git_demo_body3.sql
@@ -1,10 +1,14 @@
 create or replace PACKAGE BODY        GIT_DEMO3 AS
---V1.0 add comment
+--V1.1 add comment
   Function f1(rate float)
   RETURN float AS
   BEGIN
-    -- TODO: Initial checkin
-    RETURN NULL;
+    -- add very long logic section
+    -- long
+    -- long
+    -- logic section
+    
+    RETURN 6;
   END f1;
 
   Function f2(rate float)
