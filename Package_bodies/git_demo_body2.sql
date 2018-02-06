c258583c9c5a27fbe001d38a453fd12d59595784
Gavin Liu <gavin.liu@surrey.ca>
2018-02-06 09:37:28 -0800

    Check in on Feb 6

--- Package_bodies/git_demo_body2.sql
+++ Package_bodies/git_demo_body2.sql
@@ -6,5 +6,10 @@ create or replace PACKAGE BODY        GIT_DEMO2 AS
     -- TODO: Initial checkin
     RETURN NULL;
   END f1;
-
+  Function f2(rate Number)
+  RETURN Number AS
+  BEGIN
+    -- TODO: Initial checkin
+    RETURN NULL;
+  END f2;
 END GIT_DEMO2;
