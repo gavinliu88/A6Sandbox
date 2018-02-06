036772b91a9521b87e6f7d41cca454f1e5be7106
Gavin Liu <gavin.liu@surrey.ca>
2018-02-06 09:39:08 -0800

    Initial check in

---
+++ Packages/git_demo4.sql
@@ -0,0 +1,8 @@
+create or replace PACKAGE               GIT_DEMO4 AS 
+
+
+Function f1(rate number)
+RETURN number;
+
+
+END GIT_DEMO4;
---
+++ Packages/git_demo5.sql
@@ -0,0 +1,8 @@
+create or replace PACKAGE               GIT_DEMO5 AS 
+
+
+Function f1(rate number)
+RETURN number;
+
+
+END GIT_DEMO5;
