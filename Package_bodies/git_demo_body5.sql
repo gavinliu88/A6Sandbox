59a05e1677fdb57ed3e784aa03810b0abbebcf0a
Gavin Liu <>
2018-02-07 15:39:24 -0800

    Changed in f1

--- Package_bodies/git_demo_body5.sql
+++ Package_bodies/git_demo_body5.sql
@@ -2,9 +2,12 @@ create or replace PACKAGE BODY        GIT_DEMO5 AS
 
   Function f1(rate Number)
   RETURN Number AS
+    v_statuscode folder.statuscode%TYPE;
   BEGIN
-    -- TODO: Initial checkin
-    RETURN NULL;
-  END f1;
 
+    SELECT STATUSCODE INTO v_statuscode
+    FROM FOLDER
+    WHERE FOLDERRSN = 123;
+    RETURN v_Statuscode;
+  END f1;
 END GIT_DEMO5;
