d2a760756275e22779499b942e8be86e33160197
Gavin Liu <gavin.liu@surrey.ca>
2018-02-07 10:08:33 -0800

    Add f2

--- Package_bodies/git_demo_body8.sql
+++ Package_bodies/git_demo_body8.sql
@@ -7,4 +7,12 @@ create or replace PACKAGE BODY        GIT_DEMO8 AS
     RETURN NULL;
   END f1;
 
+
+  Function f2(rate Number)
+  RETURN Number AS
+  BEGIN
+    -- TODO: Initial checkin
+    RETURN NULL;
+  END f2;
+  
 END GIT_DEMO8;
