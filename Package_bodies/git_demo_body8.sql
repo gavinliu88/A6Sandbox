7519a82363235d42a33e4f7a79d3425907868369
Gavin Liu <gavin.liu@surrey.ca>
2018-02-07 11:01:17 -0800

    Changed in f1 and f2 in sql dev

--- Package_bodies/git_demo_body8.sql
+++ Package_bodies/git_demo_body8.sql
@@ -2,9 +2,16 @@ create or replace PACKAGE BODY        GIT_DEMO8 AS
 
   Function f1(rate Number)
   RETURN Number AS
+    v_statuscode folder.statuscode%TYPE;
   BEGIN
-    -- TODO: Initial checkin
-    RETURN NULL;
+  --Gavin add comments
+  --Gavin add comments
+    SELECT STATUSCODE INTO v_statuscode
+    FROM FOLDER
+    WHERE FOLDERRSN = 123;
+    
+    RETURN v_STATUSCODE;
+
   END f1;
 
 
@@ -12,6 +19,8 @@ create or replace PACKAGE BODY        GIT_DEMO8 AS
   RETURN Number AS
   v_fee number;
   BEGIN
+  --Gavin add comments
+  --Gavin add comments
     select lookupfee into v_fee
     from validlookup
     where lookupcode=2007
