2f306db74ad911e95eb6f0da71f82c380d7b542a
gavinliu88 <gavin.liu@surrey.ca>
2018-02-08 13:58:17 -0800

    Add f2

--- Package_bodies/git_demo_body7.sql
+++ Package_bodies/git_demo_body7.sql
@@ -13,4 +13,18 @@ create or replace PACKAGE BODY        GIT_DEMO7 AS
 
   END f1;
 
+  Function f2(rate Number)
+  RETURN Number AS
+  v_fee number;
+  BEGIN
+
+    select lookupfee into v_fee
+    from validlookup
+    where lookupcode=2007
+    and lookup1 = 24
+    and rownum = 1;
+
+    RETURN v_fee;
+  END f2;
+
 END GIT_DEMO7;
