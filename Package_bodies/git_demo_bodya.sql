eb55dbbb08229db3a58f15fa47c36a804eb4068e
gavinliu88 <gavin.liu@surrey.ca>
2018-02-09 15:38:08 -0800

    Add new function

--- Package_bodies/git_demo_bodya.sql
+++ Package_bodies/git_demo_bodya.sql
@@ -13,5 +13,31 @@ create or replace PACKAGE BODY        GIT_DEMOa AS
 
     RETURN v_fee;
   END f1;
+PROCEDURE revise_plumbing_permit (argFolderRSN IN folder.folderrsn%TYPE)
+IS
+    n_old_folderrsn     folder.folderrsn%TYPE;
+    n_count             PLS_INTEGER;
+BEGIN
+    pkc_surrey.revise_permit (argFolderRSN, n_old_folderrsn);
 
+    SELECT count(*)
+    INTO n_count
+    FROM folderprocess
+    WHERE folderrsn = n_old_folderrsn
+    AND processcode = 2190
+    AND statuscode = 1;
+
+    IF n_count = 1 THEN
+        INSERT INTO folderprocess
+                     (processrsn, folderrsn, processcode, scheduledate,
+                      statuscode, assigneduser, displayorder, mandatoryflag,
+                      stampdate, stampuser
+                     )
+        VALUES (folderprocessseq.NEXTVAL, argFolderRSN, 2190, NULL,
+                      1, 'EWSG', 15, 'N',
+                      SYSDATE, USER
+                     );
+     END IF;
+
+END revise_plumbing_permit;  
 END GIT_DEMOa;
