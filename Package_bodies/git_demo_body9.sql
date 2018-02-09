c521a831b19119cfecdb706c7626cae149fb200c
gavinliu88 <gavin.liu@surrey.ca>
2018-02-08 16:10:06 -0800

    Add f2

--- Package_bodies/git_demo_body9.sql
+++ Package_bodies/git_demo_body9.sql
@@ -14,4 +14,13 @@ create or replace PACKAGE BODY        GIT_DEMO9 AS
     RETURN v_fee;
   END f1;
 
+  Function f2(p_folderrsn Number)
+  RETURN Number AS
+    v_statuscode number;
+  BEGIN
+
+      v_rate := 6;
+    RETURN v_rate;
+  END f2;   
+
 END GIT_DEMO9;
