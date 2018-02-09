2bee218d15f50bb80cbca1e0a35f904d49fccf2f
gavin <>
2018-02-09 15:56:50 -0800

    use fee function

--- Package_bodies/git_demo_bodyb.sql
+++ Package_bodies/git_demo_bodyb.sql
@@ -5,11 +5,8 @@ create or replace PACKAGE BODY        GIT_DEMOb AS
   v_fee number;
   BEGIN
 
-    select lookupfee into v_fee
-    from validlookup
-    where lookupcode=2007
-    and lookup1 = 24
-    and rownum = 1;
+  v_fee := pkc_surrey.get_rate_with_date(2007, 24, sysdate);
+  v_feeComment := 'Minor Plumbing Field Design/Construction Revisions';
 
     RETURN v_fee;
   END f1;
