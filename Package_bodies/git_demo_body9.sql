7660fd98bf4b311ebb460b519dd69b7e77e0518a
Gavin Liu <>
2018-02-11 10:10:59 -0800

    V1.2

--- Package_bodies/git_demo_body9.sql
+++ Package_bodies/git_demo_body9.sql
@@ -6,38 +6,20 @@ create or replace PACKAGE BODY               GIT_DEMO9 AS
   v_feeComment varchar2(2000);
   BEGIN
 
-  v_fee := pkc_surrey.get_rate_with_date(2007, 24, sysdate);
-  v_feeComment := 'Minor Plumbing Field Design/Construction Revisions';
+    select lookupfee into v_fee
+    from validlookup
+    where lookupcode=2007
+    and lookup1 = 24
+    and rownum = 1;
 
     RETURN v_fee;
   END f1;
   
-PROCEDURE f2(argFolderRSN IN folder.folderrsn%TYPE)
-IS
-    n_old_folderrsn     folder.folderrsn%TYPE;
-    n_count             PLS_INTEGER;
-BEGIN
-    pkc_surrey.revise_permit (argFolderRSN, n_old_folderrsn);
-
-    SELECT count(*)
-    INTO n_count
-    FROM folderprocess
-    WHERE folderrsn = n_old_folderrsn
-    AND processcode = 2190
-    AND statuscode = 1;
-
-    IF n_count = 1 THEN
-        INSERT INTO folderprocess
-                     (processrsn, folderrsn, processcode, scheduledate,
-                      statuscode, assigneduser, displayorder, mandatoryflag,
-                      stampdate, stampuser
-                     )
-        VALUES (folderprocessseq.NEXTVAL, argFolderRSN, 2190, NULL,
-                      1, 'EWSG', 15, 'N',
-                      SYSDATE, USER
-                     );
-     END IF;
-
-END f2;  
+  Function f2(rate Number)
+  RETURN Number AS
+  BEGIN
+    -- TODO: Initial checkin
+    RETURN NULL;
+  END f2;  
    
 END GIT_DEMO9;
