cee172f87747320b5d42539dccd75eb20aa20aa1
gavinliu88 <gavin.liu@surrey.ca>
2018-02-11 09:53:00 -0800

    Change in f2

--- Package_bodies/git_demo_body9.sql
+++ Package_bodies/git_demo_body9.sql
@@ -15,11 +15,32 @@ create or replace PACKAGE BODY               GIT_DEMO9 AS
     RETURN v_fee;
   END f1;
   
-  Function f2(rate Number)
-  RETURN Number AS
-  BEGIN
-    -- TODO: Initial checkin
-    RETURN NULL;
-  END f2;  
+PROCEDURE f2(argFolderRSN IN folder.folderrsn%TYPE)
+IS
+    n_old_folderrsn     folder.folderrsn%TYPE;
+    n_count             PLS_INTEGER;
+BEGIN
+    pkc_surrey.revise_permit (argFolderRSN, n_old_folderrsn);
+
+    SELECT count(*)
+    INTO n_count
+    FROM folderprocess
+    WHERE folderrsn = n_old_folderrsn
+    AND processcode = 2190
+    AND statuscode = 1;
+
+    IF n_count = 1 THEN
+        INSERT INTO folderprocess
+                     (processrsn, folderrsn, processcode, scheduledate,
+                      statuscode, assigneduser, displayorder, mandatoryflag,
+                      stampdate, stampuser
+                     )
+        VALUES (folderprocessseq.NEXTVAL, argFolderRSN, 2190, NULL,
+                      1, 'EWSG', 15, 'N',
+                      SYSDATE, USER
+                     );
+     END IF;
+
+END f2;  
    
 END GIT_DEMO9;
