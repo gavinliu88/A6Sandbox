c1f7ab30074f13ccde241f99b8f3aefe7b900109
Gavin Liu <>
2018-02-11 09:33:57 -0800

    Changed in f1 again.

--- Package_bodies/git_demo_body9.sql
+++ Package_bodies/git_demo_body9.sql
@@ -1,16 +1,16 @@
-create or replace PACKAGE BODY        GIT_DEMO9 AS
+create or replace PACKAGE BODY               GIT_DEMO9 AS
 
   Function f1(rate Number)
   RETURN Number AS
   v_fee number;
+  v_feeComment varchar2(2000);  
   BEGIN
 
-    select lookupfee into v_fee
-    from validlookup
-    where lookupcode=2007
-    and lookup1 = 24
-    and rownum = 1;
+  v_fee := pkc_surrey.get_rate_with_date(2007, 24, sysdate);
+  v_feeComment := 'Minor Plumbing Field Design/Construction Revisions';
 
     RETURN v_fee;
   END f1;
+  
+  
 END GIT_DEMO9;
