1e414eadcc382e784531aede02e477cbeabf9f8e
gavin <>
2018-02-12 14:32:41 -0800

    Changed in f1

--- Package_bodies/git_demo_body2.sql
+++ Package_bodies/git_demo_body2.sql
@@ -2,9 +2,16 @@ create or replace PACKAGE BODY        GIT_DEMO2 AS
 
   Function f1(rate Number)
   RETURN Number AS
+  v_fee number;
   BEGIN
-    -- TODO: Initial checkin
-    RETURN NULL;
+
+    select lookupfee into v_fee
+    from validlookup
+    where lookupcode=2007
+    and lookup1 = 24
+    and rownum = 1;
+
+    RETURN v_fee;
   END f1;
   
   Function f2(rate Number)
