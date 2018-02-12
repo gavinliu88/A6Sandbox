5f52fda83049c03b798aa6c7254932c64cfae125
gavin <>
2018-02-12 14:31:29 -0800

    Changed in f1

---
+++ Package_bodies/git_demo_body2.sql
@@ -0,0 +1,17 @@
+create or replace PACKAGE BODY        GIT_DEMO2 AS
+
+  Function f1(rate Number)
+  RETURN Number AS
+  BEGIN
+    -- TODO: Initial checkin
+    RETURN NULL;
+  END f1;
+  
+  Function f2(rate Number)
+  RETURN Number AS
+  BEGIN
+    -- TODO: Initial checkin
+    RETURN NULL;
+  END f2;  
+
+END GIT_DEMO2;
