eb4990842e0b65e033d137ba46f05c542b8c12f6
gavinliu88 <gavin.liu@surrey.ca>
2018-02-12 14:49:57 -0800

    changed fee code

--- Package_bodies/git_demo_body2.sql
+++ Package_bodies/git_demo_body2.sql
@@ -5,7 +5,7 @@ create or replace PACKAGE BODY        GIT_DEMO2 AS
   v_fee number;
   BEGIN
 
-    v_fee := pkc_surrey.get_rate_with_date(2007, 24, sysdate);
+    v_fee := pkc_surrey.get_rate_with_date(2009, 56, sysdate);
     v_feeComment := 'Minor Plumbing Field Design/Construction Revisions';
 
     RETURN v_fee;
