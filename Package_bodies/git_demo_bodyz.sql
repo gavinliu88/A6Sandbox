1f876852c45e2182665b35512e6e7984cc9d8a19
Gavin Liu <>
2018-02-13 10:21:35 -0800

    Change fee code to 2008

--- Package_bodies/git_demo_bodyz.sql
+++ Package_bodies/git_demo_bodyz.sql
@@ -7,7 +7,7 @@ create or replace PACKAGE BODY        GIT_DEMO AS
   v_statuscode number;
   BEGIN
 
-  v_fee := pkc_surrey.get_rate_with_date(2007, 24, sysdate);
+  v_fee := pkc_surrey.get_rate_with_date(2008, 56, sysdate);
   v_feeComment := 'Minor Plumbing Field Design/Construction Revisions';
 
     RETURN v_fee;
