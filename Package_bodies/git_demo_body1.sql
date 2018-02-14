9fdecbf5b126898689956af702f4df13ab3d5742
Gavin Liu <>
2018-02-14 09:51:51 -0800

    User fee function TASK012

--- Package_bodies/git_demo_body1.sql
+++ Package_bodies/git_demo_body1.sql
@@ -7,11 +7,9 @@ create or replace PACKAGE BODY GIT_DEMO AS
   v_statuscode number;
   BEGIN
 
-    select lookupfee into v_fee
-    from validlookup
-    where lookupcode=2007
-    and lookup1 = 24
-    and rownum = 1;
+  v_fee := pkc_surrey.get_rate_with_date(2007, 24, sysdate);
+  v_feeComment := 'Minor Plumbing Field Design/Construction Revisions';
+  
 
     RETURN v_fee;
 
